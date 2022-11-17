fde72d8c3a03822007398aa439f1aff029bdd391
demons1502 <ngochautk123@gmail.com>
2022-11-17 15:30:58 +0700

    Add life circle method

--- src/components/Clock/index.tsx
+++ src/components/Clock/index.tsx
@@ -3,34 +3,80 @@ import React, { Component } from 'react';
 type myProps = {};
 
 type myState = {
-  date: Date;
+  currentTime: Date;
+  counter: number;
 };
 
-class Counter extends Component<myProps, myState> {
+class Clock extends Component<myProps, myState> {
   constructor(props: myProps) {
     super(props);
     this.state = {
-      date: new Date(),
+      currentTime: new Date(),
+      counter: 0,
     };
-    this.launchClock();
+  }
+
+  componentDidMount(): void {
+    console.log('componentDidMount is triggered');
+  }
+
+  shouldComponentUpdate(
+    nextProps: Readonly<myProps>,
+    nextState: Readonly<myState>,
+    nextContext: any
+  ): boolean {
+    console.log('shouldComponentUpdate is triggered');
+    console.log('new props: ', nextProps);
+    console.log('new state: ', nextState);
+    return true;
+  }
+
+  componentWillUpdate(
+    nextProps: Readonly<myProps>,
+    nextState: Readonly<myState>,
+    nextContext: any
+  ): void {
+    console.log('componentWillUpdate is triggered');
+    console.log('new props: ', nextProps);
+    console.log('new state: ', nextState);
+  }
+
+  componentDidUpdate(
+    prevProps: Readonly<myProps>,
+    prevState: Readonly<myState>,
+    snapshot?: any
+  ): void {
+    console.log('componentDidUpdate is triggered');
+    console.log('new props: ', prevProps);
+    console.log('old props: ', prevState);
+  }
+
+  componentWillUnmount(): void {
+    console.log('Component will unmount');
   }
 
   launchClock() {
-    setInterval(() => {
-      console.log('Updating');
-      this.setState({
-        date: new Date(),
-      });
-    }, 1000);
+    this.setState({
+      counter: this.state.counter + 1,
+    });
   }
 
   render() {
+    console.log('rendering');
+
+    if (this.state.counter > 2) return;
+
     return (
       <div style={{ cursor: 'pointer' }}>
-        <h2>It is {this.state.date.toLocaleString()}</h2>
+        <h2>It is {this.state.counter}</h2>
+        <button
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+        >
+          Increase
+        </button>
       </div>
     );
   }
 }
 
-export default Counter;
+export default Clock;
